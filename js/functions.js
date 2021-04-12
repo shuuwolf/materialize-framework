51c70c1e23f6622d68b1d8476b0c26578e714e70
Rafael Kauch <kauchrafael0001@gmail.com>
2021-04-12 17:24:42 -0300

    revendo

--- js/functions.js
+++ js/functions.js
@@ -3,8 +3,5 @@ $(document).ready(function(){
     $('.sidenav').sidenav();    //init sidenav//
 
     $('.modal').modal();    //init modal//
-
-    $('.modal-trigger').click(function(){
-        alert("Use senha bosta!(Ex:123, gato, rato, bolsonario)");
-    })
+    
 });
